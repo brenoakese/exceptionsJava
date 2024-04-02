400d0c0351bba87dfd1f4c3be2af740c91f9b0f4
unknown <amorimbreno1207@gmail.com>
2024-04-02 18:57:08 -0300

    final program

--- src/packageApplication/Main.java
+++ src/packageApplication/Main.java
@@ -1,7 +1,47 @@
 package packageApplication;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
+import packageExeptions.DomainException;
+import packageReservation.Reservation;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.print("ROOM NUMBER");
+            int number = leitor.nextInt();
+            System.out.print("CHECK-IN DATE (dd/MM/yyyy): ");
+            Date checkin = sdf.parse(leitor.next());
+            System.out.println("CHECK-OUT DATE (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(leitor.next());
+
+            Reservation reservation = new Reservation(number, checkin, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("ENTER DATA TO UPDATE THE RESERVATION: ");
+            System.out.print("CHECK-IN DATE (dd/MM/yyyy): ");
+            checkin = sdf.parse(leitor.next());
+            System.out.print("CHECK-OUT DATE (dd/MM/yyyy): ");
+            checkOut = sdf.parse(leitor.next());
+
+            reservation.updateDates(checkin, checkOut);
+            System.out.println("RESERVATION: " + reservation);
+
+        } catch (ParseException e) {
+            System.out.println("INVALID DATE FORMAT");
+        } catch (DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("UNEXPECTED ERROR");
+        }
+
+        leitor.close();
     }
 }
--- src/packageReservation/Reservation.java
+++ src/packageReservation/Reservation.java
@@ -4,12 +4,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import packageExeptions.DomainException;
+
 public class Reservation {
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
 
     public Integer getRoomNumber() {
         return roomNumber;
@@ -34,9 +47,20 @@ public class Reservation {
 
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+
+        Date now = new Date();
+
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+
     }
 
     @Override
