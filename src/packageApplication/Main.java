7ae8f0c9cc304794a699983e1f8b0cd74e364e18
unknown <amorimbreno1207@gmail.com>
2024-04-02 14:57:53 -0300

    classe reservation att

--- src/packageApplication/Main.java
+++ src/packageApplication/Main.java
@@ -1,5 +1,7 @@
-public class App {
+package packageApplication;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
     }
 }
